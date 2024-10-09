727aa939f5b38cdf4bd1d7084ebe7adbcf48f221
Devam42 <dkathane42@gmail.com>
2024-10-09 20:16:54 +0530

    update

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ options = Options()
 
 # Add headless mode for running without a GUI
 options.add_argument('--incognito')
-#options.add_argument('--headless')
+options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
